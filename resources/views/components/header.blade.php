0052d6e3560920e42693d4474455a166afc6c48f
Rafael Leonardi <rafael.leonardi98@gmail.com>
2023-01-13 09:20:20 +0100

    Header comp

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,27 @@
 <header>
-      <div>
-            <img src="{{ asset('') }}" alt="Logo">
-      </div>
+      {{-- Logo --}}
+    <div class="logo">
+        <img src="{{ asset('images/dc-logo') }}" alt="Logo">
+    </div>
+
+    {{-- Navbar --}}
+    <nav>
+        <ul>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">tv</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Collectibles</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">Fans</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Shop</a></li>
+        </ul>
+    </nav>
+
+    {{-- Search --}}
+    <div class="search">
+        <input type="text" placeholder="Search">
+    </div>
 </header>
